8d7961f336cad46e2dea7c755b30b75a75c3f656
Siya Sharma <ajaysh73@gmail.com>
2020-06-01 16:05:45 -0400

    Updates

---
+++ precache-manifest.c6e4543b4efb06df84d2f554a6a04a74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28b40ad2be1ffd6800462f02525867c",
+    "url": "/ReactSeasons/index.html"
+  },
+  {
+    "revision": "d5ee9397b2b018086ecc",
+    "url": "/ReactSeasons/static/css/main.44b92320.chunk.css"
+  },
+  {
+    "revision": "a1fe034d8847b2c1a104",
+    "url": "/ReactSeasons/static/js/2.f892ca35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactSeasons/static/js/2.f892ca35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ee9397b2b018086ecc",
+    "url": "/ReactSeasons/static/js/main.4d2a1135.chunk.js"
+  },
+  {
+    "revision": "f31bb67d3d53bddd2c10",
+    "url": "/ReactSeasons/static/js/runtime-main.1bf56832.js"
+  }
+]);
